be40924a60c2406f8dd8eb1b8ee42ce7e6a8cf73
Aditya Prakash <aditya.prakash132@gmail.com>
2015-12-08 22:14:58 +0530

    Add edge cases of both a's/b's being zero

--- planediv.cpp
+++ planediv.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <stdlib.h>
 
 using namespace std;
 
@@ -9,25 +10,40 @@ int main(){
   while(cases--){
     int lines, max_set_count = 1;
 
-    cin >> lines;
+    //cin >> lines;
+    lines = 5;
     int line_set[5000][3] = {0};
     for(int i=0; i<lines; i++){
       cin >> line_set[i][0] >> line_set[i][1] >> line_set[i][2];
+      // line_set[i][0] = rand()%7 - 3;
+      // line_set[i][1] = rand()%7 - 3;
+      // line_set[i][2] = rand()%7 - 3;
+      // cout << line_set[i][0]<<" " << line_set[i][1] <<" "<< line_set[i][2]<<endl;
     }
 
     for(int i=0; i<lines; i++){
+      if(line_set[i][0] == 0 && line_set[i][1] == 0) continue;
       int count = 1;
       for(int j=i; j<lines; j++){
-        // cout << "a*cur_b "<< line_set[i][0]*line_set[j][1] << " b*cur_a " << line_set[i][1]*line_set[j][0]<<endl;
-        // cout << "c*cur_a "<< line_set[i][2]*line_set[j][0] << " a*cur_c " << line_set[j][2]*line_set[i][0]<<endl;
-        // cout << endl;
-        if(line_set[i][0]*line_set[j][1] == line_set[i][1]*line_set[j][0] && (line_set[i][2]*line_set[j][0] != line_set[j][2]*line_set[i][0])) count++;
+        cout << "a*cur_b "<< line_set[i][0]*line_set[j][1] << " b*cur_a " << line_set[i][1]*line_set[j][0]<<endl;
+        cout << "c*cur_a "<< line_set[i][2]*line_set[j][0] << " a*cur_c " << line_set[j][2]*line_set[i][0]<<endl;
+        cout << endl;
+        if(line_set[i][0] == 0 && line_set[j][0] == 0){
+          if((line_set[i][1] == line_set[j][1]) && (line_set[i][2]*line_set[j][1] != line_set[j][2]*line_set[i][1])) count++;
+          continue;
+        }
+        else if(line_set[i][1] == 0 && line_set[j][1] == 0){
+          if((line_set[i][0] == line_set[j][0]) && (line_set[i][2]*line_set[j][0] != line_set[j][2]*line_set[i][0])) count++;
+          continue;
+        }
+
+        if(line_set[i][0]*line_set[j][1] == line_set[i][1]*line_set[j][0] && ((line_set[i][2]*line_set[j][0] != line_set[j][2]*line_set[i][0]) || (line_set[i][2]*line_set[j][1] != line_set[j][2]*line_set[i][1]))) count++;
       }
       if(count > max_set_count) max_set_count = count;
-      // cout << endl;
-      // cout << endl;
+      cout << endl;
+      cout << endl;
     }
-    cout << max_set_count << endl;
+    cout << max_set_count << endl<<endl;
   }
   return 0;
 }
